de33aa71176119c456a5608918fa9eb34d329a3f
Yoshimura Yuu <yyu@mental.poker>
2017-08-09 21:17:46 +0900

    fix

--- HigherKinded/main.swift
+++ HigherKinded/main.swift
@@ -1,7 +1,7 @@
 print( [1, 2, 3, 4, 5].mapF({ (x: Int) -> Int in return x + 1 }).prj() )
 
 let tree: Tree<Int> =
-    Tree<Int>.Node(
+    .Node(
         l: .Node(l: .Leaf, a: 1, r: .Leaf),
         a: 2,
         r: .Node(l: .Leaf, a: 3, r: .Leaf)
